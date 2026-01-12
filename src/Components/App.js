6bfd17ac9268f1d8a5759adf3283545d2b60353a
Debasish Ghosh <debasishghosh20032025@gmail.com>
2026-01-12 23:01:10 +0530

    fix: persist contacts using lazy state initialization

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,16 +1,36 @@
-import React  ,{useState} from 'react';
+import React  ,{useState,useEffect} from 'react';
 import './App.css';
 import Header from './Header';
 import AddContact from './AddContact';  
 import ContactList from './ContactList';
 
 function App() {
+  const LOCAL_KEY_STORAGE="contacts";
+  
 
-  const [contacts,setcontacts]=useState([])
+  const [contacts, setcontacts] = useState(() => {
+  const stored = localStorage.getItem(LOCAL_KEY_STORAGE);
+  return stored ? JSON.parse(stored) : [];
+});
+
   const addContactHandeler=(contact)=>{
      
       setcontacts([...contacts,contact]);
-  }
+  };
+
+  useEffect(()=>{
+  localStorage.setItem(LOCAL_KEY_STORAGE,JSON.stringify(contacts));
+},[contacts])
+
+
+useEffect(() => {
+  const storedContacts = JSON.parse(
+    localStorage.getItem(LOCAL_KEY_STORAGE)
+  );
+  if (storedContacts) setcontacts(storedContacts);
+}, []);
+
+
 
   return (
     <div className='ui container'> 
